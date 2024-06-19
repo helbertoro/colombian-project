6cc925408fca93b3e58a54e06b3725f642591529
Helber Fabian Toro <helber.toro@gmail.com>
2024-06-19 12:34:39 -0500

    install analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-8S3J4X31GY" />
+    </>
+  )
 }
