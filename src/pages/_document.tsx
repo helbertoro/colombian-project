7cc2ab59f432536be4e7100f623a9a5975ef6ee2
Helber Fabian Toro <helber.toro@gmail.com>
2024-09-25 06:57:14 -0500

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon-16x16.png"
+        />
+        <link rel="icon" href="/static/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
